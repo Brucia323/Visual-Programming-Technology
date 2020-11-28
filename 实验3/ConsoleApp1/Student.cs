cbb8a003b11c4b112132a9ca9ede7456e19e0d36
周晨阳 <1448517833@qq.com>
2020-11-28 13:45:50 +0800

    new

---
+++ 实验3/ConsoleApp1/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        string name;
+        string sex;
+        int age;
+        string major;
+        public Student(string name, string sex, int age, string major)
+        {
+            this.name = name;//姓名
+            this.sex = sex;//性别
+            this.age = age;//年龄
+            this.major = major;//专业
+        }
+        public string disname()
+        {
+            return name;
+        }
+        public string dissex()
+        {
+            return sex;
+        }
+        public int disage()
+        {
+            return age;
+        }
+        public string dismajor()
+        {
+            return major;
+        }
+        ~Student() { }
+    }
+}
---
+++ 实验3/ConsoleApp4/Rectangular.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Rectangular:Graphics
+    {
+        public int Area(int a,int b)
+        {
+            return a * b;
+        }
+    }
+}
