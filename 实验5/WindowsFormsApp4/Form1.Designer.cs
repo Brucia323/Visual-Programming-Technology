3f53498d97421a4733e80486784cb8a806638d09
周晨阳 <1448517833@qq.com>
2020-11-28 13:51:29 +0800

    new

---
+++ 实验5/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(282, 450);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(288, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(512, 450);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "资源管理器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ 实验5/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode treeNode = treeView1.SelectedNode;
+            listView1.Items.Clear();
+            foreach (TreeNode treeNode1 in treeNode.Nodes)
+                listView1.Items.Add(treeNode1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add("第一章 C#语言概述");
+            treeView1.Nodes[0].Nodes.Add("1.1 什么是C#语言");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("1.1.1 C#语言的发展历程");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("1.1.2 C#语言的特点");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("1.1.3 用C#编写的应用程序类型");
+            treeView1.Nodes[0].Nodes.Add("1.2 .NET Framework");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("1.2.1 什么是.NET Framework");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("1.2.2 开发托管代码的过程");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("1.2.3 C#语言与.NET Framework");
+            treeView1.Nodes[0].Nodes.Add("1.3 Visual Studio 2012的安装、启动和退出");
+            treeView1.Nodes[0].Nodes[2].Nodes.Add("1.3.1 Visual Studio 2012的安装");
+            treeView1.Nodes[0].Nodes[2].Nodes.Add("1.3.2 配置Visual C#开发环境");
+            treeView1.Nodes[0].Nodes[2].Nodes.Add("1.3.3 Visual Studio 2012的启动");
+            treeView1.Nodes[0].Nodes[2].Nodes.Add("1.3.4 Visual Studio 2012的退出");
+            treeView1.Nodes[0].Nodes.Add("1.4 Visual C#集成开发环境");
+            treeView1.Nodes[0].Nodes[3].Nodes.Add("1.4.1 启动Visual C#集成开发环境");
+            treeView1.Nodes[0].Nodes[3].Nodes.Add("1.4.2 Visual C#的菜单栏");
+            treeView1.Nodes[0].Nodes[3].Nodes.Add("1.4.3 Visual C#的工具栏");
+            treeView1.Nodes[0].Nodes[3].Nodes.Add("1.4.4 解决方案资源管理器");
+            treeView1.Nodes[0].Nodes[3].Nodes.Add("1.4.5 编辑器的设置");
+            treeView1.Nodes[0].Nodes.Add("1.5 一个简单的C#程序");
+            treeView1.Nodes[0].Nodes[4].Nodes.Add("1.5.1 代码分析");
+            treeView1.Nodes[0].Nodes[4].Nodes.Add("1.5.2 项目的构成");
+            treeView1.Nodes[0].Nodes[4].Nodes.Add("1.5.3 控制台应用程序中的基本元素");
+        }
+    }
+}
