ac8063212ae0a7a249b82322583197e4dec346e5
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2020-12-03 18:51:47 +0800

    update

--- 实验6/学生选课-成绩管理系统/Form1.Designer.cs
+++ 实验6/学生选课-成绩管理系统/Form1.Designer.cs
@@ -29,20 +29,113 @@ namespace 学生选课_成绩管理系统
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(324, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "院系信息管理";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(324, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "专业、方向信息管理";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(324, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "班级信息管理";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(324, 213);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 25);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "学生信息管理";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(324, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 25);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "课程信息管理";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Location = new System.Drawing.Point(324, 307);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 25);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "教师信息管理";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Location = new System.Drawing.Point(324, 354);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 25);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "设置";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "学生选课/成绩管理系统";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
