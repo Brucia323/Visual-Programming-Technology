a7fd734bc88337e187c367215d40013eb7f032d9
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2020-12-06 14:46:07 +0800

    update

--- 实验6/学生选课-成绩管理系统/学生选课和成绩管理系统.Designer.cs
+++ 实验6/学生选课-成绩管理系统/学生选课和成绩管理系统.Designer.cs
@@ -47,6 +47,7 @@ namespace 学生选课_成绩管理系统
             this.button1.TabIndex = 0;
             this.button1.Text = "院系信息管理";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -57,6 +58,7 @@ namespace 学生选课_成绩管理系统
             this.button2.TabIndex = 1;
             this.button2.Text = "专业、方向信息管理";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -67,6 +69,7 @@ namespace 学生选课_成绩管理系统
             this.button3.TabIndex = 2;
             this.button3.Text = "班级信息管理";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -77,6 +80,7 @@ namespace 学生选课_成绩管理系统
             this.button4.TabIndex = 3;
             this.button4.Text = "学生信息管理";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -87,6 +91,7 @@ namespace 学生选课_成绩管理系统
             this.button5.TabIndex = 4;
             this.button5.Text = "课程信息管理";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -97,6 +102,7 @@ namespace 学生选课_成绩管理系统
             this.button6.TabIndex = 5;
             this.button6.Text = "教师信息管理";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
--- 实验6/学生选课-成绩管理系统/学生选课和成绩管理系统.cs
+++ 实验6/学生选课-成绩管理系统/学生选课和成绩管理系统.cs
@@ -16,5 +16,41 @@ namespace 学生选课_成绩管理系统
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            院系信息管理 yx = new 院系信息管理();
+            yx.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            专业_方向信息管理 zy_fx = new 专业_方向信息管理();
+            zy_fx.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            班级信息管理 bj = new 班级信息管理();
+            bj.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            学生信息管理 xs = new 学生信息管理();
+            xs.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            课程信息管理 kc = new 课程信息管理();
+            kc.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            教师信息管理 js = new 教师信息管理();
+            js.Show();
+        }
     }
 }
