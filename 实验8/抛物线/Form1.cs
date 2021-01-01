dac164d4f916948af827efcd9e9e86d8b2465b32
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-01-01 13:02:42 +0800

    update

--- 实验8/抛物线/Form1.cs
+++ 实验8/抛物线/Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace 抛物线
@@ -19,7 +12,7 @@ namespace 抛物线
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics graphics = this.CreateGraphics();
+            Graphics graphics = CreateGraphics();
             graphics.DrawArc(Pens.Black, 60, 200, 600, 300, 190, 160);
         }
     }
