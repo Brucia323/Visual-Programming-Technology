0eee794bd338f568e585cb6f1be47fe41911660e
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-01-01 16:38:23 +0800

    update

--- 实验8/动态时钟/Form1.cs
+++ 实验8/动态时钟/Form1.cs
@@ -1,28 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace 动态时钟
 {
     public partial class Form1 : Form
     {
-        Timer Timer;//定义时钟，定时重新绘制
-        Graphics Graphics;//创建画布
-        Pen Pen;//创建画笔
-        int width;//画布高度
-        int height;//画布宽度
+        private Graphics Graphics;//创建画布
+        private Pen Pen;//创建画笔
+        private int width;//画布高度
+        private int height;//画布宽度
         public Form1()
         {
             InitializeComponent();
-            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
-            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.ResizeRedraw, true);
             timer1.Tick += new EventHandler(timer1_Tick);
         }
         protected override void OnPaint(PaintEventArgs e)
@@ -31,8 +24,8 @@ namespace 动态时钟
             Graphics = e.Graphics;
             Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;//抗锯齿
             Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;//高质量
-            width = this.Width - 75;//时钟宽度
-            height = this.Height - 75;//时钟高度
+            width = Width - 75;//时钟宽度
+            height = Height - 75;//时钟高度
             int x1 = 0;//起点坐标
             int y1 = 0;//起点坐标
             Graphics.FillEllipse(new SolidBrush(Color.Black), x1 + 2, y1 + 2, width, height);//表盘背景
@@ -57,24 +50,28 @@ namespace 动态时钟
             Pen = new Pen(Color.Yellow, 1);
             Graphics.RotateTransform(6 * second);
             float y = (float)((-1) * (height / 2.75));
-            Graphics.DrawLine(Pen, new PointF(0, 0), new PointF((float)0, y));
-            Pen = new Pen(Color.White, 4);
-            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            Graphics.DrawLine(Pen, new PointF(0, 0), new PointF(0, y));
+            Pen = new Pen(Color.White, 4)
+            {
+                EndCap = System.Drawing.Drawing2D.LineCap.Round
+            };
             Graphics.RotateTransform(-6 * second);
             Graphics.RotateTransform((float)(second * 0.1 + minute * 6));
             y = (float)((-1) * ((height - 30) / 2.75));
-            Graphics.DrawLine(Pen, new PointF(0, 0), new PointF((float)0, y));
-            Pen = new Pen(Color.White, 6);
-            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            Graphics.DrawLine(Pen, new PointF(0, 0), new PointF(0, y));
+            Pen = new Pen(Color.White, 6)
+            {
+                EndCap = System.Drawing.Drawing2D.LineCap.Round
+            };
             Graphics.RotateTransform((float)(-second * 0.1 - minute * 6));
             Graphics.RotateTransform((float)(second * 0.01 + minute * 0.1 + hour * 30));
             y = (float)((-1) * ((height - 45) / 2.75));
-            Graphics.DrawLine(Pen, new PointF(0, 0), new PointF((float)0, y));
+            Graphics.DrawLine(Pen, new PointF(0, 0), new PointF(0, y));
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            this.Invalidate();
+            Invalidate();
         }
     }
 }
