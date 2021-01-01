b3c822255bd2cfb3f2e4d18fa574da7c6916c37b
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-01-01 10:57:24 +0800

    Create

---
+++ 实验8/抛物线/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 抛物线
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = this.CreateGraphics();
+            graphics.DrawArc(Pens.Black, 60, 200, 600, 300, 190, 160);
+        }
+    }
+}
